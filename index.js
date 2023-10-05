b96083a3b3212f14d550f76b6ab0ecb82d6fcd5e
Brandi Burnell <brandiburnell6@gmail.com>
2023-10-04 19:50:36 -0600

    passed all tests

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // code your solution here
+
+function superbowlWin(record) {
+
+    let superBowlWinYear;
+
+    function isWinningYear(element, index, array) {
+        return (element.result === "W");
+    }
+
+    let ret = record.find(isWinningYear);
+    if (ret === undefined) {
+        return undefined;
+    } else{
+        return ret.year;
+    }
+
+}
+
+
+// function superbowlWin(element) {
+//     console.log("year", element.year);
+//     console.log("result", element.result);
+
+//     if (element.result === "W") {
+//         return element.year;
+//     } else {
+//         return undefined;
+//     }
+// }
+
+
+
